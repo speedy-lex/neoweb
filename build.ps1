ec92898fa5f3e8cab076044543e43bc03439f4cf
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-07-26 12:59:37 +0200

    more libc shims

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 $env:RUSTFLAGS="-Zfmt-debug=none -Zlocation-detail=none"
 cargo +nightly build --release #-Z build-std=core,alloc -Z build-std-features="optimize_for_size"
 Remove-Item env:RUSTFLAGS
-wasm-snip target/wasm32-unknown-unknown/release/neoweb.wasm -o pkg/snipped.wasm
-wasm-opt --enable-bulk-memory-opt -Oz --strip-dwarf --vacuum --strip-debug -n pkg/snipped.wasm -o pkg/neoweb.wasm
+# wasm-snip doesnt know about wasm-eh for now
+# wasm-snip target/wasm32-unknown-unknown/release/neoweb.wasm -o pkg/snipped.wasm
+wasm-opt --enable-bulk-memory-opt -Oz --strip-dwarf --vacuum --strip-debug -n target/wasm32-unknown-unknown/release/neoweb.wasm -o pkg/neoweb.wasm
