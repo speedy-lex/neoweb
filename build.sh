4ef5c2d4e2d9b972a0b44addab2744a9c969b38d
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-07-31 00:16:50 +0300

    computer implemementation

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+RUSTFLAGS="-Zfmt-debug=none -Zlocation-detail=none" cargo +nightly build --release
+wasm-opt --enable-bulk-memory-opt --enable-exception-handling -Oz --strip-dwarf --vacuum --strip-debug -n target/wasm32-unknown-unknown/release/neoweb.wasm -o pkg/neoweb.wasm
